f6fa291bd026a082b5cea5ab5eb4c954c5d00ac7
Alvis12 <107262011+Alvis12@users.noreply.github.com>
2023-11-05 16:38:35 +0800

    vs1

---
+++ 電子書/storybook.js
@@ -0,0 +1,39 @@
+// storybook.js
+
+const totalPages = 10;  // 总页数
+let currentPage = 1;
+
+const prevPageButton = document.getElementById('prev-page');
+const nextPageButton = document.getElementById('next-page');
+
+function showPages(currentPage) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach((page, index) => {
+        const isDisplayed = index >= currentPage - 1 && index <= currentPage; // 显示当前页和下一页的内容
+        page.style.display = isDisplayed ? 'block' : 'none';
+    });
+}
+
+nextPageButton.addEventListener('click', () => {
+    if (currentPage + 2 <= totalPages) {
+        currentPage += 2; // 翻两页
+        prevPageButton.disabled = false;
+        showPages(currentPage);
+        if (currentPage + 2 >= totalPages) {
+            nextPageButton.disabled = true;
+        }
+    }
+});
+
+prevPageButton.addEventListener('click', () => {
+    if (currentPage > 1) {
+        currentPage -= 2; // 翻两页
+        nextPageButton.disabled = false;
+        showPages(currentPage);
+        if (currentPage === 1) {
+            prevPageButton.disabled = true;
+        }
+    }
+});
+
+showPages(currentPage);
